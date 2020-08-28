f96d7f9302c9dadc8c0f76f3d50641bbaa3352b6
Johnny OMara <omarajohnj@gmail.com>
2020-08-28 15:40:32 -0500

    just trying stuff

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ require('./socket')(io)
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, '../public/index.html'))
-// })
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 server.listen(8081, function() {
   console.log(`Listening on ${server.address().port}`)
